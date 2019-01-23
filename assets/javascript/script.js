daf34d9cda56a9f2baa4f40af5c4f8ba1373d0b6
ArchangelD <sdimartino@gmail.com>
2019-01-23 01:24:41 -0800

    Adds API query

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,6 +8,18 @@ $(document).ready(function() {
 
     //When the user clicks on one of the buttons, the page grabs 10 static, non-animated gif images from the GIPHY API related to that topic and places them on the page. Each GIFs rating appears below it.
 
+    var apiKey = "UJKxGxreBmxXae04g503IlVBamFlm4Db";
+    var queryURL = "https://api.giphy.com/v1/gifs/?api_key=" + apiKey;
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+
+
+
+      });
+
     //When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
 
     //Adds a form to the page that takes the value from a user input box and adds it into the `topics` array.
