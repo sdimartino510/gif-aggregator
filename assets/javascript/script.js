5c355d273801994809cd8d41769b302f0b6c45cf
ArchangelD <sdimartino@gmail.com>
2019-01-23 08:54:36 -0800

    Adds gif search and play/pause functionality

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,81 +1,144 @@
 $(document).ready(function() {
+  //Creates an array of strings which will be topics for GIF searches, in this case, baseball teams.
 
-    //Creates an array of strings which will be topics for GIF searches, in this case, baseball teams.
+  var topics = [
+    "Oakland A's",
+    "Houston Astros",
+    "Los Angeles Angels",
+    "Seattle Mariners",
+    "Texas Rangers"
+  ];
 
-    var topics = ["Oakland A's", "Houston Astros", "Los Angeles Angels", "Seattle Mariners", "Texas Rangers"];
+  //Creates buttons for each topic
 
-    //Creates buttons for each topic
+  function createButtons() {
+    // Delete the content inside the button-box div prior to adding new buttons
 
-    function createButtons() {
+    $(".button-box").empty();
 
-        // Delete the content inside the button-box div prior to adding new buttons
+    // Loop through the array of movies, then generate buttons for each movie in the array
 
-        $(".button-box").empty();
+    for (var i = 0; i < topics.length; i++) {
+      var gifButton = $("<button>");
 
-        // Loop through the array of movies, then generate buttons for each movie in the array
+      gifButton.addClass("gif-button");
 
-        for (var i = 0; i < topics.length; i++) {
+      gifButton.attr("data-type", topics[i]);
 
-          var gifButton = $("<button>");
+      gifButton.text(topics[i]);
 
-          gifButton.addClass("gif-button gif gif-button-color");
+      $(".button-box").append(gifButton);
+    }
+  }
 
-          gifButton.text(topics[i]);
+  // This function handles events where the add gif button is clicked
 
-          $(".button-box").append(gifButton);
+  $("#add-gif").on("click", function(event) {
+    // event.preventDefault() prevents submit button from trying to send a form.
 
-        }
+    event.preventDefault();
 
+    // This code grabs the text the user types into the input field
+
+    const userInput = $("#gif-input")
+      .val()
+      .trim();
+
+    // This code adds the new gif topic into the topics array
+
+    if (!topics.includes(userInput)) {
+      topics.push(userInput);
     }
 
-    // This function handles events where the add gif button is clicked
-
-    $("#add-gif").on("click", function(event) {
-
-        // event.preventDefault() prevents submit button from trying to send a form.
-        
-        event.preventDefault();
-
-        // This code grabs the text the user types into the input field
-
-        const userInput = $("#gif-input").val().trim();
-
-        // This code adds the new gif topic into the topics array
-
-        if (!topics.includes(userInput)) {
-
-            topics.push(userInput);
-
-        }
-
-        // The createButtons function is called, creating the list of gif buttons
-
-        createButtons();
-
-    });
-
-    // Calling the createButtons function to display the initial list of topics
+    // The createButtons function is called, creating the list of gif buttons
 
     createButtons();
 
-    //When the user clicks on one of the buttons, the page grabs 10 static, non-animated gif images from the GIPHY API related to that topic and places them on the page. Each GIFs rating appears below it.
+    $("#gif-input").val("");
+
+  });
+
+  // Calling the createButtons function to display the initial list of topics
+
+  createButtons();
+
+  //When the user clicks on one of the buttons, the page grabs 10 static, non-animated gif images from the GIPHY API related to that topic and places them on the page. Each GIFs rating appears below it.
+
+  $(document).on("click", ".gif-button", function() {
+    $(".gif-box").empty();
+
+    var type = $(this).data("type");
 
     var apiKey = "UJKxGxreBmxXae04g503IlVBamFlm4Db";
-    var queryURL = "https://api.giphy.com/v1/gifs/?api_key=" + apiKey;
+    var queryURL =
+      "https://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=" + apiKey;
 
     $.ajax({
-        url: queryURL,
-        method: "GET"
+      url: queryURL,
+      method: "GET"
     }).then(function(response) {
 
+        console.log(response);
 
+        for (var i = 0; i < response.data.length; i++) {
+
+            var searchDiv = $("<div class='search-item'>");
+
+            var rating = response.data[i].rating;
+
+            var p = $("<h3>").text("Rating: " + rating);
+
+            var animated = response.data[i].images.fixed_height.url;
+
+            var still = response.data[i].images.fixed_height_still.url;
+
+            var img = $("<img>");
+
+            img.attr("src", still);
+
+            img.attr("data-still", still);
+
+            img.attr("data-animated", animated);
+
+            img.attr("data-state", "still");
+
+            img.addClass("search-gif");
+
+            searchDiv.append(img, p);
+
+            // searchDiv.append(img);
+
+            $(".gif-box").append(searchDiv);
+
+        }
 
     });
 
-    //When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+  });
 
-    //Adds a form to the page that takes the value from a user input box and adds it into the `topics` array.
+  $(document).on("click", ".search-gif", function() {
 
-    //Makes a function call that takes each topic in the array remakes the buttons on the page.
+    var state = $(this).attr("data-state");
 
+    if (state === "still") {
+
+        $(this).attr("src", $(this).data("animated"));
+
+        $(this).attr("data-state", "animated");
+
+    } else if (state === "animated") {
+
+        $(this).attr("src", $(this).data("still"));
+
+        $(this).attr("data-state", "still");
+
+    }
+
+  })
+
+  //When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+
+  //Adds a form to the page that takes the value from a user input box and adds it into the `topics` array.
+
+  //Makes a function call that takes each topic in the array remakes the buttons on the page.
 });
