d2cca647bc3a28a3ee33643b1d6d49943e868ff7
ArchangelD <sdimartino@gmail.com>
2019-01-23 01:17:01 -0800

    Adds pseudocode and begins adding JS code

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,11 +1,17 @@
-//Creates an array of strings which will be topics for GIF searches, in this case, baseball teams.
+$(document).ready(function() {
 
-//Creates buttons for each topic
+    //Creates an array of strings which will be topics for GIF searches, in this case, baseball teams.
 
-//When the user clicks on one of the buttons, the page grabs 10 static, non-animated gif images from the GIPHY API related to that topic and places them on the page. Each GIFs rating appears below it.
+    var topics = ["Oakland A's", "Houston Astros", "Los Angeles Angels", "Seattle Mariners", "Texas Rangers"];
 
-//When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+    //Creates buttons for each topic
 
-//Adds a form to the page that takes the value from a user input box and adds it into the `topics` array.
+    //When the user clicks on one of the buttons, the page grabs 10 static, non-animated gif images from the GIPHY API related to that topic and places them on the page. Each GIFs rating appears below it.
 
-//Makes a function call that takes each topic in the array remakes the buttons on the page.
+    //When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+
+    //Adds a form to the page that takes the value from a user input box and adds it into the `topics` array.
+
+    //Makes a function call that takes each topic in the array remakes the buttons on the page.
+
+});
