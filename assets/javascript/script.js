01eada0218c00793542f801bd82d1c2fd2c68e7f
ArchangelD <sdimartino@gmail.com>
2019-01-23 01:15:27 -0800

    Adds pseudocode and begins adding JS code

---
+++ assets/javascript/script.js
@@ -0,0 +1,11 @@
+//Creates an array of strings which will be topics for GIF searches, in this case, baseball teams.
+
+//Creates buttons for each topic
+
+//When the user clicks on one of the buttons, the page grabs 10 static, non-animated gif images from the GIPHY API related to that topic and places them on the page. Each GIFs rating appears below it.
+
+//When the user clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
+
+//Adds a form to the page that takes the value from a user input box and adds it into the `topics` array.
+
+//Makes a function call that takes each topic in the array remakes the buttons on the page.
